527b61008dc3e6afcf45d462a79540a7f9b8ecdd
Adhyatman <adhyatman101@gmail.com>
2026-04-23 19:59:33 +0530

    Initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
+  return (
+    <h1>Check console for backend response</h1>
+  );
+}
+
+export default App;
